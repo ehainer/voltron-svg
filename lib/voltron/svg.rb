063c5d08ab715f9f5f7c6f88fd071873b87648ad
Eric Hainer <eric.hainer@gmail.com>
2016-09-29 17:06:16 -0600

    Removes legacy debugging

--- lib/voltron/svg.rb
+++ lib/voltron/svg.rb
@@ -9,18 +9,15 @@ module Voltron
 
     module SassHelpers
       def svg_icon(source, options={})
-        # Remove whitespace and quotes from beginning and end of source
-        source = source.to_s.gsub(/(^[\s"']*)|(["'\s]*$)/, "")
-
         # Remove whitespace and quotes from beginning and end of each option value
         options = options.symbolize_keys.compact.map { |k,v| { k => v.to_s.gsub(/(^[\s"']*)|(["'\s]*$)/, "") } }.reduce(Hash.new, :merge)
 
-        tag = Voltron::Svg::Tag.new(source, options)
+        tag = Voltron::Svg::Tag.new(source.value, options)
 
         ::Sass::Script::String.new "url(\"#{tag.image_path}\");\nbackground-image: url(\"#{tag.svg_path}\"), linear-gradient(transparent, transparent)"
       end
 
-      ::Sass::Script::Functions.declare :svg_icon, [], var_args: true, var_kwargs: true
+      ::Sass::Script::Functions.declare :svg_icon, [:source, :options], var_args: false, var_kwargs: true
     end
 
     module ViewHelpers
