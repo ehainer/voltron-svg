b4ce185bac9285e77877dff247703f232b83a27e
Eric Hainer <eric.hainer@gmail.com>
2016-09-29 16:31:10 -0600

    Cleaning up

--- lib/voltron/svg.rb
+++ lib/voltron/svg.rb
@@ -11,17 +11,16 @@ module Voltron
       def svg_icon(source, options={})
         # Remove whitespace and quotes from beginning and end of source
         source = source.to_s.gsub(/(^[\s"']*)|(["'\s]*$)/, "")
-        source << ".svg" unless source.ends_with?(".svg")
 
         # Remove whitespace and quotes from beginning and end of each option value
         options = options.symbolize_keys.compact.map { |k,v| { k => v.to_s.gsub(/(^[\s"']*)|(["'\s]*$)/, "") } }.reduce(Hash.new, :merge)
 
         tag = Voltron::Svg::Tag.new(source, options)
 
-        Sass::Script::Value::String.new "url(\"#{tag.image_path}\");\nbackground-image: url(\"#{tag.svg_path}\"), linear-gradient(transparent, transparent)"
+        ::Sass::Script::String.new "url(\"#{tag.image_path}\");\nbackground-image: url(\"#{tag.svg_path}\"), linear-gradient(transparent, transparent)"
       end
 
-      Sass::Script::Functions.declare :svg_icon, [], var_args: true, var_kwargs: true
+      ::Sass::Script::Functions.declare :svg_icon, var_kwargs: true
     end
 
     module ViewHelpers
