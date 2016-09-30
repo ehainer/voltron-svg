1e1f0cdae275f80ba3d3270dd24ff19b49c253d4
Eric Hainer <eric.hainer@gmail.com>
2016-09-29 22:29:24 -0600

    Refines specs

--- lib/voltron/svg.rb
+++ lib/voltron/svg.rb
@@ -14,12 +14,11 @@ module Voltron
         ::Sass::Script::String.new "url(\"#{tag.image_path}\");\nbackground-image: url(\"#{tag.svg_path}\"), linear-gradient(transparent, transparent);\nbackground-size: #{tag.width}px #{tag.height}px"
       end
 
-      protected
-        def map_options(options={})
-          ::Sass::Util.map_hash(options) do |key, value|
-            [key.to_sym, (value.respond_to?(:representation) ? value.representation : (value.respond_to?(:value) ? value.value : value))]
-          end
+      def map_options(options={})
+        ::Sass::Util.map_hash(options) do |key, value|
+          [key.to_sym, (value.respond_to?(:representation) ? value.representation : (value.respond_to?(:value) ? value.value : value))]
         end
+      end
 
       ::Sass::Script::Functions.declare :svg_icon, [:source], var_kwargs: true
     end
--- spec/voltron/svg_spec.rb
+++ spec/voltron/svg_spec.rb
@@ -12,11 +12,16 @@ describe Voltron::Svg do
   end
 
   it "should generate an img tag for given svg" do
-    expect(svg_tag(:airplane)).to match(/^<img.*/i)
+    expect(svg_tag(::Sass::Script::Value::String.new("airplane"))).to match(/^<img.*/i)
   end
 
   it "should output background css for svg fallback" do
-    expect(svg_icon(:airplane)).to_not be_blank
+    expect(svg_icon(::Sass::Script::Value::String.new("airplane"))).to_not be_blank
+  end
+
+  it "should convert sass value arguments to a hash" do
+    options = { "size" => ::Sass::Script::Value::String.new("50x50", :string), "color" => ::Sass::Script::Value::Color.new([255, 255, 255], "white") }
+    expect(map_options(options)).to eq({ size: "50x50", color: "white" })
   end
 
 end
